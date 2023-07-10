48eb1e5e71e6e7416f14c103878136e72e6d14d8
KarimS <KarimS@KARIM>
2023-07-09 20:26:25 -0600

    Add project files.

---
+++ Byte_Coffee/CustomControls/BindablePasswordBox.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Byte_Coffee.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for BindablePasswordBox.xaml
+    /// </summary>
+    public partial class BindablePasswordBox : UserControl
+    {
+        private static readonly DependencyProperty passwordProperty =
+        DependencyProperty.Register("Password", typeof(SecureString), typeof(BindablePasswordBox));
+
+        public SecureString Password
+        {
+            get { return (SecureString)GetValue(passwordProperty); }
+            set { SetValue(passwordProperty, value); }
+        }
+
+        public BindablePasswordBox()
+        {
+            InitializeComponent();
+            txtPassword.PasswordChanged += OnpasswordChanged;
+        }
+
+        private void OnpasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = txtPassword.SecurePassword;
+
+        }
+    }
+}
---
+++ Byte_Coffee/DB/Condb.cs
@@ -0,0 +1,57 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Byte_Coffee.DB
+{
+    public class Condb
+    {
+        NpgsqlConnection conexion = new NpgsqlConnection();
+        static String servidor = "mahmud.db.elephantsql.com";
+        static String nombre_base_datos = "xchvzdjj";
+        static String usuario = "xchvzdjj";
+        static String clave = "bPUPz1PviGoQ0n9vFAdM7mcydNGMkhDk";
+        static String puerto = "5432";
+        String urlConexion = "server=" + servidor + ";" + "port=" + puerto + ";" + "user id=" + usuario + ";" + "password=" + clave + ";" + "database=" + nombre_base_datos + ";";
+
+        public NpgsqlConnection EstablecerConexion()
+        {
+
+            try
+            {
+                conexion.ConnectionString = urlConexion;
+                conexion.Open();
+
+            }
+
+            catch (NpgsqlException e)
+            {
+                MessageBox.Show("No se pudo conectar a la base de datos de PostgreSQL, error: " + e.ToString());
+
+            }
+
+            return conexion;
+        }
+        public NpgsqlConnection CerrarConexion()
+        {
+
+            try
+            {
+                conexion.Close();
+
+            }
+
+            catch (NpgsqlException e)
+            {
+                MessageBox.Show("No se pudo cerrar la conexion a la base de datos de PostgreSQL, error: " + e.ToString());
+
+            }
+
+            return conexion;
+        }
+    }
+}
